425b9ca0124715b8f3c6ef86925c46c8f9908115
David Del Val <ddelvalfraire@outlook.com>
2026-01-31 10:40:40 -0800

    feat: set up Docker configuration and environment for Infisical integration

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  generateBuildId: async () => {
+    return process.env.GIT_HASH || 'build-id-not-set';
+  },
 };
 
 export default nextConfig;
