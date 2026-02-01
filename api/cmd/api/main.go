3960c492538db5efe845ef1fc4214537a8094844
David Del Val <ddelvalfraire@outlook.com>
2026-02-01 01:10:50 -0800

    feat: Set up structured logging with `slog` and integrate HTTP request logging middleware.

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/Ask-Atlas/AskAtlas/api/internal/logging"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+	logger := logging.NewLogger()
+	slog.SetDefault(logger)
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(logging.RequestLogger(logger))
+	r.Use(middleware.Recoverer)
+
+	// TODO: Add timeout to a config file instead of hardcoding
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello World"))
 	})
-	http.ListenAndServe(":8080", router)
+
+	addr := ":8080"
+	slog.Info("Server starting", "addr", addr)
+	if err := http.ListenAndServe(addr, r); err != nil {
+		slog.Error("Server failed to start", "error", err)
+		os.Exit(1)
+	}
 }
